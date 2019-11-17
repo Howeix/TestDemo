e610cfe6ab400766f4ad00ec5822c0fb9bd93a5e
Howeix <495658492@qq.com>
2019-11-17 22:28:30 +0800

    test

---
+++ test/test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by Jack on 2019/11/17.
+//  Copyright © 2019 Jack. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
